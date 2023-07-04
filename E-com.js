135e19ca77eebae1f6700f26b2ae24ccfb4a9b9d
Layowolo Joy Ibipiri <120032751+tljhgf@users.noreply.github.com>
2023-07-04 14:07:47 +0100

    Add files via upload

---
+++ E-com.js
@@ -0,0 +1,28 @@
+let dropBtn= document.querySelector('.hamburger');
+let dropContent= document.querySelector('.nav-content');
+
+function mouseOver(){
+    // document.querySelector('.hamburger')
+    // classList.toggle('show')
+    // dropBtn.style
+    dropContent.classList.toggle('show');
+    console.log('working');
+}
+btn.addEventListener("click",e=>{
+    if (yes3.style.display === '') {
+            hamburger.style.display="flex"
+    yes3.style.display="none"
+    yes1.style.transform="rotate(45deg)"
+    yes2.style.transform="rotate(-45deg)"
+    yes2.style.marginTop="-8px"
+    } else {
+      hamburger.style.display="none"
+    yes3.style.display=""
+    yes1.style.transform="rotate(0deg)"
+    yes2.style.transform="rotate(-0deg)"
+    yes2.style.marginTop="-0px"
+    }
+
+  })
+
+          
